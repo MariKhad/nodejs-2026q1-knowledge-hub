b29366169baaf6a241ac0c444236b7775f852e86
MariKhad <utabond89@gmail.com>
2026-04-02 16:30:02 +0300

    created all 4 modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { CommentModule } from './comment/comment.module';
+import { CategoryModule } from './category/category.module';
+import { UserModule } from './user/user.module';
+import { ArticleModule } from './article/article.module';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ArticleModule, UserModule, CategoryModule, CommentModule],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
